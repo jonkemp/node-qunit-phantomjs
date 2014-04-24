e7b2abc879ee7af7a92a5fee6f2c9519562f5e72
Jonathan Kemp <jonathan.kemp@cellularsales.com>
2014-04-24 11:33:26 -0400

    Refactor to use as a module.

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 'use strict';
 var path = require('path');
 var childProcess = require('child_process');
-var argv = require('minimist')(process.argv.slice(2));
 var phantomjs = require('phantomjs');
 var binPath = phantomjs.path;
-var runner;
 
-if (!argv.reporter || argv.reporter === 'min') {
-	runner = 'lib/runner.js';
-} else if (argv.reporter === 'list') {
-	runner = 'lib/runner-list.js';
-}
-
-var childArgs = [
-    path.join(__dirname, runner),
-    argv._[0]
-];
-
-childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
-    console.log(stdout);
-
-    if (stderr !== '') {
-        console.log(stderr);
+module.exports = function (filepath, options) {
+    var opt = options || {};
+    var runner = 'lib/runner.js';
+    if (opt.verbose) {
+        runner = 'lib/runner-list.js';
     }
 
-    if (err !== null) {
-        console.log(err);
-    }
-});
+    var absolutePath = path.resolve(filepath),
+        isAbsolutePath = absolutePath.indexOf(filepath) >= 0;
+
+    var childArgs = [
+        path.join(__dirname, runner),
+        (isAbsolutePath ? 'file:///' + absolutePath.replace(/\\/g, '/') : filepath)
+    ];
+
+    return childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
+        console.log('Testing ' + path.relative(__dirname, filepath));
+
+        if (stdout) {
+            stdout = stdout.trim(); // Trim trailing cr-lf
+            console.log(stdout);
+        }
+
+        if (stderr) {
+            console.log(stderr);
+        }
+
+        if (err) {
+            console.log(err);
+        }
+    });
+};
