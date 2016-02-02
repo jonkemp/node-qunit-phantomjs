159a8007e50e0bed1793d8892ce4ff6aef16a5e4
Jonathan Kemp <kempdogg@gmail.com>
2016-02-01 22:54:42 -0500

    Fix eslint issues.

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ var path = require('path'),
 module.exports = function (filepath, options, callback) {
     var opt = options || {},
         cb = callback || function () {},
-        runner = path.join(phantomjsRunnerDir, 'runner-json.js');
+        runner = path.join(phantomjsRunnerDir, 'runner-json.js'),
+        absolutePath = path.resolve(filepath),
+        isAbsolutePath = absolutePath.indexOf(filepath) >= 0,
+        childArgs = [],
+        proc;
 
     if (opt.verbose) {
         runner = path.join(phantomjsRunnerDir, 'runner-list.js');
@@ -20,12 +24,8 @@ module.exports = function (filepath, options, callback) {
         runner = opt.customRunner;
     }
 
-    var absolutePath = path.resolve(filepath),
-        isAbsolutePath = absolutePath.indexOf(filepath) >= 0,
-        childArgs = [];
-
     if (opt['phantomjs-options'] && opt['phantomjs-options'].length) {
-        childArgs.push( opt['phantomjs-options'] );
+        childArgs.push(opt['phantomjs-options']);
     }
 
     childArgs.push(
@@ -33,21 +33,23 @@ module.exports = function (filepath, options, callback) {
         (isAbsolutePath ? 'file:///' + absolutePath.replace(/\\/g, '/') : filepath)
     );
 
-    if ( opt.timeout ) {
-        childArgs.push( opt.timeout );
+    if (opt.timeout) {
+        childArgs.push(opt.timeout);
     }
 
-    var proc = childProcess.execFile(binPath, childArgs, function (err, stdout, stderr) {
-        console.log('Testing ' + chalk.blue( path.relative(__dirname, filepath) ));
+    proc = childProcess.execFile(binPath, childArgs, function (err, stdout, stderr) {
+        var out,
+            result,
+            message,
+            output;
+
+        console.log('Testing ' + chalk.blue(path.relative(__dirname, filepath)));
 
         if (stdout) {
             try {
-                var out,
-                    result,
-                    message,
-                    output;
-
                 stdout.trim().split('\n').forEach(function (line) {
+                    var test;
+
                     try {
                         out = JSON.parse(line.trim());
                         result = out.result;
@@ -59,13 +61,12 @@ module.exports = function (filepath, options, callback) {
                         console.log(output);
 
                         if (out.exceptions) {
-                            for (var test in out.exceptions) {
+                            for (test in out.exceptions) {
                                 console.log('\n' + chalk.red('Test failed') + ': ' + chalk.red(test) + ': \n' + out.exceptions[test].join('\n  '));
                             }
                         }
                     } catch (e) {
-                        line = line.trim(); // Trim trailing cr-lf
-                        console.log(line);
+                        console.log(line.trim());
                     }
                 });
             } catch (e) {
