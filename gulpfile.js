aa05c44fb6b39fca3d8bd1e1c819041130008b47
Jonathan Kemp <kempdogg@gmail.com>
2018-10-27 20:52:40 -0400

    Update to gulp 4.0.

--- gulpfile.js
+++ gulpfile.js
@@ -4,37 +4,36 @@
 var gulp = require('gulp'),
     eslint = require('gulp-eslint'),
     mocha = require('gulp-mocha'),
-    qunit = require('./index');
+    qunit = require('./index'),
+    paths = {
+        scripts: ['./*.js', '!./gulpfile.js']
+    };
 
-var paths = {
-    scripts: ['./*.js', '!./gulpfile.js']
-};
-
-gulp.task('lint', function() {
+gulp.task('lint', function () {
     return gulp.src(paths.scripts)
         .pipe(eslint())
         .pipe(eslint.format());
 });
 
-gulp.task('test', function() {
+gulp.task('test', function () {
     return gulp.src('./test/*.js')
         .pipe(mocha());
 });
 
-gulp.task('qunit:pass', function() {
+gulp.task('qunit:pass', function () {
     qunit('./test/fixtures/passing.html');
 });
 
-gulp.task('qunit:fail', function() {
+gulp.task('qunit:fail', function () {
     qunit('./test/fixtures/failing.html');
 });
 
-gulp.task('qunit-verbose', function() {
+gulp.task('qunit-verbose', function () {
     qunit('./test/fixtures/passing.html', { 'verbose': true });
 });
 
 gulp.task('watch', function () {
-    gulp.watch(paths.scripts, ['lint', 'jscs', 'test']);
+    gulp.watch(paths.scripts, gulp.parallel('lint', 'jscs', 'test'));
 });
 
-gulp.task('default', ['lint', 'jscs', 'test', 'watch']);
+gulp.task('default', gulp.parallel('lint', 'jscs', 'test', 'watch'));
